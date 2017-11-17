4085691e89e2c0ef0cf0411173d3e4692b2a723f
Chandler Morrison <cwm795@gmail.com>
2017-11-17 09:19:15 -0600

    committing changes

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("hey there");
+
+function createProduct(fairyName, fairyDescription, fairyPrice, fairyImage) {
+    let product = {};
+
+    product.fairyName = fairyName;
+    product.fairyDescription = fairyDescription;
+    product.fairyPrice = fairyPrice;
+    product.fairyImage = fairyImage;
+
+    return product;
+}
+
+var tinkerbell = ("Tinkerbell", "Our most popular fairy", "How much you got?", "img/tinkerbell.jpg")
+
+var listOfFairies = (tinkerbell);
+
+ let htmlBlock = "";
+// for (i = 0; listOfFairies.length; i += 1);
+
+console.log(html.block, "test");
